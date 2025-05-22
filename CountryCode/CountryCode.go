db9586e9338f76c0377bdb2caf033bad4a90a828
golangframework <server.ozga@gmail.com>
2025-05-22 09:44:27 +0200

    Country codes.

---
+++ CountryCode/CountryCode.go
@@ -0,0 +1,613 @@
+package CountryCode
+
+type Enum string
+
+const (
+	Afghanistan                  Enum = "AF"
+	Albania                      Enum = "AL"
+	Algeria                      Enum = "DZ"
+	Andorra                      Enum = "AD"
+	Angola                       Enum = "AO"
+	AntiguaAndBarbuda            Enum = "AG"
+	Argentina                    Enum = "AR"
+	Armenia                      Enum = "AM"
+	Australia                    Enum = "AU"
+	Austria                      Enum = "AT"
+	Azerbaijan                   Enum = "AZ"
+	Bahamas                      Enum = "BS"
+	Bahrain                      Enum = "BH"
+	Bangladesh                   Enum = "BD"
+	Barbados                     Enum = "BB"
+	Belarus                      Enum = "BY"
+	Belgium                      Enum = "BE"
+	Belize                       Enum = "BZ"
+	Benin                        Enum = "BJ"
+	Bhutan                       Enum = "BT"
+	Bolivia                      Enum = "BO"
+	BosniaAndHerzegovina         Enum = "BA"
+	Botswana                     Enum = "BW"
+	Brazil                       Enum = "BR"
+	BruneiDarussalam             Enum = "BN"
+	Bulgaria                     Enum = "BG"
+	BurkinaFaso                  Enum = "BF"
+	Burundi                      Enum = "BI"
+	Cambodia                     Enum = "KH"
+	Cameroon                     Enum = "CM"
+	Canada                       Enum = "CA"
+	CapeVerde                    Enum = "CV"
+	CentralAfricanRepublic       Enum = "CF"
+	Chad                         Enum = "TD"
+	Chile                        Enum = "CL"
+	China                        Enum = "CN"
+	Colombia                     Enum = "CO"
+	Comoros                      Enum = "KM"
+	Congo                        Enum = "CG"
+	CostaRica                    Enum = "CR"
+	Croatia                      Enum = "HR"
+	Cuba                         Enum = "CU"
+	Cyprus                       Enum = "CY"
+	Czechia                      Enum = "CZ"
+	DemocraticRepublicOfTheCongo Enum = "CD"
+	Denmark                      Enum = "DK"
+	Djibouti                     Enum = "DJ"
+	Dominica                     Enum = "DM"
+	DominicanRepublic            Enum = "DO"
+	Ecuador                      Enum = "EC"
+	Egypt                        Enum = "EG"
+	ElSalvador                   Enum = "SV"
+	EquatorialGuinea             Enum = "GQ"
+	Eritrea                      Enum = "ER"
+	Estonia                      Enum = "EE"
+	Eswatini                     Enum = "SZ"
+	Ethiopia                     Enum = "ET"
+	Fiji                         Enum = "FJ"
+	Finland                      Enum = "FI"
+	France                       Enum = "FR"
+	Gabon                        Enum = "GA"
+	Gambia                       Enum = "GM"
+	Georgia                      Enum = "GE"
+	Germany                      Enum = "DE"
+	Ghana                        Enum = "GH"
+	Greece                       Enum = "GR"
+	Grenada                      Enum = "GD"
+	Guatemala                    Enum = "GT"
+	Guinea                       Enum = "GN"
+	GuineaBissau                 Enum = "GW"
+	Guyana                       Enum = "GY"
+	Haiti                        Enum = "HT"
+	Honduras                     Enum = "HN"
+	Hungary                      Enum = "HU"
+	Iceland                      Enum = "IS"
+	India                        Enum = "IN"
+	Indonesia                    Enum = "ID"
+	Iran                         Enum = "IR"
+	Iraq                         Enum = "IQ"
+	Ireland                      Enum = "IE"
+	Israel                       Enum = "IL"
+	Italy                        Enum = "IT"
+	IvoryCoast                   Enum = "CI"
+	Jamaica                      Enum = "JM"
+	Japan                        Enum = "JP"
+	Jordan                       Enum = "JO"
+	Kazakhstan                   Enum = "KZ"
+	Kenya                        Enum = "KE"
+	Kiribati                     Enum = "KI"
+	Kuwait                       Enum = "KW"
+	Kyrgyzstan                   Enum = "KG"
+	Lao                          Enum = "LA"
+	Latvia                       Enum = "LV"
+	Lebanon                      Enum = "LB"
+	Lesotho                      Enum = "LS"
+	Liberia                      Enum = "LR"
+	Libia                        Enum = "LY"
+	Liechtenstein                Enum = "LI"
+	Lithuania                    Enum = "LT"
+	Luxembourg                   Enum = "LU"
+	Madagascar                   Enum = "MG"
+	Malawi                       Enum = "MW"
+	Malaysia                     Enum = "MY"
+	Maldives                     Enum = "MV"
+	Mali                         Enum = "ML"
+	Malta                        Enum = "MT"
+	MarshallIslands              Enum = "MH"
+	Mauritania                   Enum = "MR"
+	Mauritius                    Enum = "MU"
+	Mexico                       Enum = "MX"
+	Micronesia                   Enum = "FM"
+	Moldova                      Enum = "MD"
+	Monaco                       Enum = "MC"
+	Mongolia                     Enum = "MN"
+	Montenegro                   Enum = "ME"
+	Morocco                      Enum = "MA"
+	Mozambique                   Enum = "MZ"
+	Myanmar                      Enum = "MM"
+	Namibia                      Enum = "NA"
+	Nauru                        Enum = "NR"
+	Nepal                        Enum = "NP"
+	Netherlands                  Enum = "NL"
+	NewZealand                   Enum = "NZ"
+	Nicaragua                    Enum = "NI"
+	Niger                        Enum = "NE"
+	Nigeria                      Enum = "NG"
+	NorthKorea                   Enum = "KP"
+	NorthMacedonia               Enum = "MK"
+	Norway                       Enum = "NO"
+	Oman                         Enum = "OM"
+	Pakistan                     Enum = "PK"
+	Palau                        Enum = "PW"
+	Panama                       Enum = "PA"
+	PapuaNewGuinea               Enum = "PG"
+	Paraguay                     Enum = "PY"
+	Peru                         Enum = "PE"
+	Philippines                  Enum = "PH"
+	Poland                       Enum = "PL"
+	Portugal                     Enum = "PT"
+	Qatar                        Enum = "QA"
+	Romania                      Enum = "RO"
+	RussianFederation            Enum = "RU"
+	Rwanda                       Enum = "RW"
+	SaintKittsAndNevis           Enum = "KN"
+	SaintLucia                   Enum = "LC"
+	SaintVincentAndTheGrenadines Enum = "VC"
+	Samoa                        Enum = "WS"
+	SanMarino                    Enum = "SM"
+	SaoTomeAndPrincipe           Enum = "ST"
+	SaudiArabia                  Enum = "SA"
+	Senegal                      Enum = "SN"
+	Serbia                       Enum = "RS"
+	Seychelles                   Enum = "SC"
+	SierraLeone                  Enum = "SL"
+	Singapore                    Enum = "SG"
+	Slovakia                     Enum = "SK"
+	Slovenia                     Enum = "SI"
+	SolomonIslands               Enum = "SB"
+	Somalia                      Enum = "SO"
+	SouthAfrica                  Enum = "ZA"
+	SouthKorea                   Enum = "KR"
+	SouthSudan                   Enum = "SS"
+	Spain                        Enum = "ES"
+	SriLanka                     Enum = "LK"
+	Sudan                        Enum = "SD"
+	Suriname                     Enum = "SR"
+	Sweden                       Enum = "SE"
+	Switzerland                  Enum = "CH"
+	Syria                        Enum = "SY"
+	Tajikistan                   Enum = "TJ"
+	Tanzania                     Enum = "TZ"
+	Thailand                     Enum = "TH"
+	TimorLeste                   Enum = "TL"
+	Togo                         Enum = "TG"
+	Tonga                        Enum = "TO"
+	TrinidatAndTobago            Enum = "TT"
+	Tunisia                      Enum = "TN"
+	Turkey                       Enum = "TR"
+	Turkmenistan                 Enum = "TM"
+	Tuvalu                       Enum = "TV"
+	Uganda                       Enum = "UG"
+	Ukraine                      Enum = "UA"
+	UnitedArabEmirates           Enum = "AE"
+	UnitedKingdom                Enum = "GB"
+	UnitedStates                 Enum = "US"
+	Uruguay                      Enum = "UY"
+	Uzbekistan                   Enum = "UZ"
+	Vanuatu                      Enum = "VU"
+	VaticanCity                  Enum = "VA"
+	Venezuela                    Enum = "VE"
+	Vietnam                      Enum = "VN"
+	Yemen                        Enum = "YE"
+	Zambia                       Enum = "ZM"
+	Zimbabwe                     Enum = "ZW"
+)
+
+var names map[Enum]string = map[Enum]string{
+	Afghanistan:                  "AF",
+	Albania:                      "AL",
+	Algeria:                      "DZ",
+	Andorra:                      "AD",
+	Angola:                       "AO",
+	AntiguaAndBarbuda:            "AG",
+	Argentina:                    "AR",
+	Armenia:                      "AM",
+	Australia:                    "AU",
+	Austria:                      "AT",
+	Azerbaijan:                   "AZ",
+	Bahamas:                      "BS",
+	Bahrain:                      "BH",
+	Bangladesh:                   "BD",
+	Barbados:                     "BB",
+	Belarus:                      "BY",
+	Belgium:                      "BE",
+	Belize:                       "BZ",
+	Benin:                        "BJ",
+	Bhutan:                       "BT",
+	Bolivia:                      "BO",
+	BosniaAndHerzegovina:         "BA",
+	Botswana:                     "BW",
+	Brazil:                       "BR",
+	BruneiDarussalam:             "BN",
+	Bulgaria:                     "BG",
+	BurkinaFaso:                  "BF",
+	Burundi:                      "BI",
+	Cambodia:                     "KH",
+	Cameroon:                     "CM",
+	Canada:                       "CA",
+	CapeVerde:                    "CV",
+	CentralAfricanRepublic:       "CF",
+	Chad:                         "TD",
+	Chile:                        "CL",
+	China:                        "CN",
+	Colombia:                     "CO",
+	Comoros:                      "KM",
+	Congo:                        "CG",
+	CostaRica:                    "CR",
+	Croatia:                      "HR",
+	Cuba:                         "CU",
+	Cyprus:                       "CY",
+	Czechia:                      "CZ",
+	DemocraticRepublicOfTheCongo: "CD",
+	Denmark:                      "DK",
+	Djibouti:                     "DJ",
+	Dominica:                     "DM",
+	DominicanRepublic:            "DO",
+	Ecuador:                      "EC",
+	Egypt:                        "EG",
+	ElSalvador:                   "SV",
+	EquatorialGuinea:             "GQ",
+	Eritrea:                      "ER",
+	Estonia:                      "EE",
+	Eswatini:                     "SZ",
+	Ethiopia:                     "ET",
+	Fiji:                         "FJ",
+	Finland:                      "FI",
+	France:                       "FR",
+	Gabon:                        "GA",
+	Gambia:                       "GM",
+	Georgia:                      "GE",
+	Germany:                      "DE",
+	Ghana:                        "GH",
+	Greece:                       "GR",
+	Grenada:                      "GD",
+	Guatemala:                    "GT",
+	Guinea:                       "GN",
+	GuineaBissau:                 "GW",
+	Guyana:                       "GY",
+	Haiti:                        "HT",
+	Honduras:                     "HN",
+	Hungary:                      "HU",
+	Iceland:                      "IS",
+	India:                        "IN",
+	Indonesia:                    "ID",
+	Iran:                         "IR",
+	Iraq:                         "IQ",
+	Ireland:                      "IE",
+	Israel:                       "IL",
+	Italy:                        "IT",
+	IvoryCoast:                   "CI",
+	Jamaica:                      "JM",
+	Japan:                        "JP",
+	Jordan:                       "JO",
+	Kazakhstan:                   "KZ",
+	Kenya:                        "KE",
+	Kiribati:                     "KI",
+	Kuwait:                       "KW",
+	Kyrgyzstan:                   "KG",
+	Lao:                          "LA",
+	Latvia:                       "LV",
+	Lebanon:                      "LB",
+	Lesotho:                      "LS",
+	Liberia:                      "LR",
+	Libia:                        "LY",
+	Liechtenstein:                "LI",
+	Lithuania:                    "LT",
+	Luxembourg:                   "LU",
+	Madagascar:                   "MG",
+	Malawi:                       "MW",
+	Malaysia:                     "MY",
+	Maldives:                     "MV",
+	Mali:                         "ML",
+	Malta:                        "MT",
+	MarshallIslands:              "MH",
+	Mauritania:                   "MR",
+	Mauritius:                    "MU",
+	Mexico:                       "MX",
+	Micronesia:                   "FM",
+	Moldova:                      "MD",
+	Monaco:                       "MC",
+	Mongolia:                     "MN",
+	Montenegro:                   "ME",
+	Morocco:                      "MA",
+	Mozambique:                   "MZ",
+	Myanmar:                      "MM",
+	Namibia:                      "NA",
+	Nauru:                        "NR",
+	Nepal:                        "NP",
+	Netherlands:                  "NL",
+	NewZealand:                   "NZ",
+	Nicaragua:                    "NI",
+	Niger:                        "NE",
+	Nigeria:                      "NG",
+	NorthKorea:                   "KP",
+	NorthMacedonia:               "MK",
+	Norway:                       "NO",
+	Oman:                         "OM",
+	Pakistan:                     "PK",
+	Palau:                        "PW",
+	Panama:                       "PA",
+	PapuaNewGuinea:               "PG",
+	Paraguay:                     "PY",
+	Peru:                         "PE",
+	Philippines:                  "PH",
+	Poland:                       "PL",
+	Portugal:                     "PT",
+	Qatar:                        "QA",
+	Romania:                      "RO",
+	RussianFederation:            "RU",
+	Rwanda:                       "RW",
+	SaintKittsAndNevis:           "KN",
+	SaintLucia:                   "LC",
+	SaintVincentAndTheGrenadines: "VC",
+	Samoa:                        "WS",
+	SanMarino:                    "SM",
+	SaoTomeAndPrincipe:           "ST",
+	SaudiArabia:                  "SA",
+	Senegal:                      "SN",
+	Serbia:                       "RS",
+	Seychelles:                   "SC",
+	SierraLeone:                  "SL",
+	Singapore:                    "SG",
+	Slovakia:                     "SK",
+	Slovenia:                     "SI",
+	SolomonIslands:               "SB",
+	Somalia:                      "SO",
+	SouthAfrica:                  "ZA",
+	SouthKorea:                   "KR",
+	SouthSudan:                   "SS",
+	Spain:                        "ES",
+	SriLanka:                     "LK",
+	Sudan:                        "SD",
+	Suriname:                     "SR",
+	Sweden:                       "SE",
+	Switzerland:                  "CH",
+	Syria:                        "SY",
+	Tajikistan:                   "TJ",
+	Tanzania:                     "TZ",
+	Thailand:                     "TH",
+	TimorLeste:                   "TL",
+	Togo:                         "TG",
+	Tonga:                        "TO",
+	TrinidatAndTobago:            "TT",
+	Tunisia:                      "TN",
+	Turkey:                       "TR",
+	Turkmenistan:                 "TM",
+	Tuvalu:                       "TV",
+	Uganda:                       "UG",
+	Ukraine:                      "UA",
+	UnitedArabEmirates:           "AE",
+	UnitedKingdom:                "GB",
+	UnitedStates:                 "US",
+	Uruguay:                      "UY",
+	Uzbekistan:                   "UZ",
+	Vanuatu:                      "VU",
+	VaticanCity:                  "VA",
+	Venezuela:                    "VE",
+	Vietnam:                      "VN",
+	Yemen:                        "YE",
+	Zambia:                       "ZM",
+	Zimbabwe:                     "ZW",
+}
+
+func (enum Enum) String() string {
+	if name, ok := names[enum]; ok {
+		return name
+	}
+	return "Unknown"
+}
+
+func IsDefined(enum Enum) bool {
+	_, ok := names[enum]
+	return ok
+}
+
+func (enum Enum) IsDefined() bool {
+	_, ok := names[enum]
+	return ok
+}
+
+func GetValues() []Enum {
+	return []Enum{
+		Afghanistan,
+		Albania,
+		Algeria,
+		Andorra,
+		Angola,
+		AntiguaAndBarbuda,
+		Argentina,
+		Armenia,
+		Australia,
+		Austria,
+		Azerbaijan,
+		Bahamas,
+		Bahrain,
+		Bangladesh,
+		Barbados,
+		Belarus,
+		Belgium,
+		Belize,
+		Benin,
+		Bhutan,
+		Bolivia,
+		BosniaAndHerzegovina,
+		Botswana,
+		Brazil,
+		BruneiDarussalam,
+		Bulgaria,
+		BurkinaFaso,
+		Burundi,
+		Cambodia,
+		Cameroon,
+		Canada,
+		CapeVerde,
+		CentralAfricanRepublic,
+		Chad,
+		Chile,
+		China,
+		Colombia,
+		Comoros,
+		Congo,
+		CostaRica,
+		Croatia,
+		Cuba,
+		Cyprus,
+		Czechia,
+		DemocraticRepublicOfTheCongo,
+		Denmark,
+		Djibouti,
+		Dominica,
+		DominicanRepublic,
+		Ecuador,
+		Egypt,
+		ElSalvador,
+		EquatorialGuinea,
+		Eritrea,
+		Estonia,
+		Eswatini,
+		Ethiopia,
+		Fiji,
+		Finland,
+		France,
+		Gabon,
+		Gambia,
+		Georgia,
+		Germany,
+		Ghana,
+		Greece,
+		Grenada,
+		Guatemala,
+		Guinea,
+		GuineaBissau,
+		Guyana,
+		Haiti,
+		Honduras,
+		Hungary,
+		Iceland,
+		India,
+		Indonesia,
+		Iran,
+		Iraq,
+		Ireland,
+		Israel,
+		Italy,
+		IvoryCoast,
+		Jamaica,
+		Japan,
+		Jordan,
+		Kazakhstan,
+		Kenya,
+		Kiribati,
+		Kuwait,
+		Kyrgyzstan,
+		Lao,
+		Latvia,
+		Lebanon,
+		Lesotho,
+		Liberia,
+		Libia,
+		Liechtenstein,
+		Lithuania,
+		Luxembourg,
+		Madagascar,
+		Malawi,
+		Malaysia,
+		Maldives,
+		Mali,
+		Malta,
+		MarshallIslands,
+		Mauritania,
+		Mauritius,
+		Mexico,
+		Micronesia,
+		Moldova,
+		Monaco,
+		Mongolia,
+		Montenegro,
+		Morocco,
+		Mozambique,
+		Myanmar,
+		Namibia,
+		Nauru,
+		Nepal,
+		Netherlands,
+		NewZealand,
+		Nicaragua,
+		Niger,
+		Nigeria,
+		NorthKorea,
+		NorthMacedonia,
+		Norway,
+		Oman,
+		Pakistan,
+		Palau,
+		Panama,
+		PapuaNewGuinea,
+		Paraguay,
+		Peru,
+		Philippines,
+		Poland,
+		Portugal,
+		Qatar,
+		Romania,
+		RussianFederation,
+		Rwanda,
+		SaintKittsAndNevis,
+		SaintLucia,
+		SaintVincentAndTheGrenadines,
+		Samoa,
+		SanMarino,
+		SaoTomeAndPrincipe,
+		SaudiArabia,
+		Senegal,
+		Serbia,
+		Seychelles,
+		SierraLeone,
+		Singapore,
+		Slovakia,
+		Slovenia,
+		SolomonIslands,
+		Somalia,
+		SouthAfrica,
+		SouthKorea,
+		SouthSudan,
+		Spain,
+		SriLanka,
+		Sudan,
+		Suriname,
+		Sweden,
+		Switzerland,
+		Syria,
+		Tajikistan,
+		Tanzania,
+		Thailand,
+		TimorLeste,
+		Togo,
+		Tonga,
+		TrinidatAndTobago,
+		Tunisia,
+		Turkey,
+		Turkmenistan,
+		Tuvalu,
+		Uganda,
+		Ukraine,
+		UnitedArabEmirates,
+		UnitedKingdom,
+		UnitedStates,
+		Uruguay,
+		Uzbekistan,
+		Vanuatu,
+		VaticanCity,
+		Venezuela,
+		Vietnam,
+		Yemen,
+		Zambia,
+		Zimbabwe,
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thereisnoplanb/globalization
+
+go 1.24.0
